45a9a5b9a25be5af963977965a912f032287dc6d
magisterha <caramessanchez@hotmail.com>
2025-09-18 20:45:55 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: '"Amare" 是什麼意思？',
+        answers: [
+            { text: '愛', correct: true },
+            { text: '恨', correct: false },
+            { text: '走路', correct: false },
+            { text: '吃', correct: false }
+        ]
+    },
+    {
+        question: '"Canis" 是什麼意思？',
+        answers: [
+            { text: '貓', correct: false },
+            { text: '馬', correct: false },
+            { text: '狗', correct: true },
+            { text: '獅子', correct: false }
+        ]
+    },
+    {
+        question: '"Aqua" 是什麼意思？',
+        answers: [
+            { text: '火', correct: false },
+            { text: '水', correct: true },
+            { text: '土', correct: false },
+            { text: '空氣', correct: false }
+        ]
+    },
+    {
+        question: '"Magister" 是什麼意思？',
+        answers: [
+            { text: '學生', correct: false },
+            { text: '士兵', correct: false },
+            { text: '國王', correct: false },
+            { text: '老師', correct: true }
+        ]
+    },
+    {
+        question: '"Lupus" 是什麼意思？',
+        answers: [
+            { text: '獅子', correct: false },
+            { text: '熊', correct: false },
+            { text: '狼', correct: true },
+            { text: '老鷹', correct: false }
+        ]
+    },
+    {
+        question: '"Domus" 是什麼意思？',
+        answers: [
+            { text: '房子', correct: true },
+            { text: '學校', correct: false },
+            { text: '寺廟', correct: false },
+            { text: '城市', correct: false }
+        ]
+    },
+    {
+        question: '"Puella" 是什麼意思？',
+        answers: [
+            { text: '男孩', correct: false },
+            { text: '女人', correct: false },
+            { text: '男人', correct: false },
+            { text: '女孩', correct: true }
+        ]
+    },
+    {
+        question: '"Sol" 是什麼意思？',
+        answers: [
+            { text: '月亮', correct: false },
+            { text: '星星', correct: false },
+            { text: '太陽', correct: true },
+            { text: '天空', correct: false }
+        ]
+    },
+    {
+        question: '"Vinum" 是什麼意思？',
+        answers: [
+            { text: '水', correct: false },
+            { text: '麵包', correct: false },
+            { text: '葡萄酒', correct: true },
+            { text: '牛奶', correct: false }
+        ]
+    },
+    {
+        question: '"Bellum" 是什麼意思？',
+        answers: [
+            { text: '和平', correct: false },
+            { text: '愛', correct: false },
+            { text: '勝利', correct: false },
+            { text: '戰爭', correct: true }
+        ]
+    }
+];
